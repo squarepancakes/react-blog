f1d5d2c3c7f76914e1a2b5a5a5295d45f6da7505
squarepancakes <goh.shuyun@gmail.com>
2019-09-13 18:17:28 +0800

    tidying

--- react-blog.js
+++ react-blog.js
@@ -1,34 +1,19 @@
 const blogPost = document.querySelector(".blog-post");
 
-// class Post extends React.Component {
-// 	constructor() {
-// 		super();
-// 		this.state = {
-// 			blogA: "A",
-// 			blogB: "B",
-// 			blogC: "C"
-// 		};
-// 	}
-// 	render() {
-// 		<div>
-// 			<Post name="A" by="A" date="A" />
-// 		</div>;
-// 	}
-// }
-
-function Blog() {
+function BlogFrog() {
 	return (
 		<article>
-			<Post name="A" by="A" date="A" />
-			<Post name="B" by="B" date="B" />
-			<Post name="C" by="C" date="C" />
+			<Post title="A" by="A" date="A" />
+			<Post title="B" by="B" date="B" />
+			<Post title="C" by="C" date="C" />
 		</article>
 	);
 }
+
 function Post(props) {
 	return (
 		<section>
-			<h1>This is {props.name}</h1>
+			<h1>This is {props.title}</h1>
 			<p>Written by {props.by}</p>
 			<p>On {props.date}</p>
 		</section>
